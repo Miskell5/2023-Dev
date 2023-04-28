851f12c1752b6615d24e5f40587582fb19e75358
Miskell5 <tatansito57@gmail.com>
2023-04-27 19:35:22 -0500

    Modificación de clase creada, comentarios y referencias para documentacion

--- 2023-Brayan/Cursos 2023/Java Básico/project1/src/Lecciones/Ejercicios.java
+++ 2023-Brayan/Cursos 2023/Java Básico/project1/src/Lecciones/Ejercicios.java
@@ -1,4 +1,25 @@
 package Lecciones;
 
+/**
+ * Clase principal, punto de entrada para la aplicación
+ * @author Brayan Horta
+ */
+
 public class Ejercicios {
+    /** Función de entrada
+     * @param args Argumentos a recibir
+     * @since 1.0
+     */
+    public static void main(String[] args) {
+
+        //Comentario de una línea
+        /*
+        Comentario de varias lineas
+         */
+        // La funcion de comentarios es ayudar a identificar el código
+
+        // imprimir texto en pantalla
+        System.out.println("Hola Mundo");
+    }
+
 }
